65d9ac2d850cf7137cf705c878df7c700328f94b
tugaybogdan <tugaybogdan8@gmail.com>
2023-10-26 14:08:52 +0300

    Add commit

---
+++ board/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+
+from .models import BoardMessage
+
+class BoardMessageForm(ModelForm):
+    class Meta:
+        model = BoardMessage
+        fields = ('title', 'content', 'price', 'rubric')
+        
--- board/views.py
+++ board/views.py
@@ -1,4 +1,41 @@
-from django.http import HttpResponse
+from typing import Any
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+from .forms import BoardMessageForm
+from .models import BoardMessage, Rubric
+
+class BoardMessageCreateView(CreateView):
+        template_name = 'board/create.html'
+        form_class = BoardMessageForm
+        success_url = reverse_lazy('index')
+
+        def get_context_data(self, **kwargs) -> dict[str, Any]:
+                context =  super().get_context_data(**kwargs)
+                context["rubrics"] = Rubric.objects.all()
+                return context
+        
 
 def index(request):
-    return HttpResponse("Тут буде список оголошень.") 
+        posts = BoardMessage.objects.all()
+        rubrics = Rubric.objects.all()
+        context = {
+                'post': posts,
+                'rubrics': rubrics,
+        }
+        context = {'posts': posts}
+        return render(request, 'board/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+        posts = BoardMessage.objects.filter(rubric = rubric_id)
+        rubrics = Rubric.objects.all()
+        current_rubric = Rubric.objects.get(pk=rubric_id)
+        #context = {
+        #        'post': posts,
+        #        'rubrics': rubrics,
+        #        'current_rubrics': current_rubric,
+        #}
+        #return render(request, 'board/by_rubric.html', context)
+        return render(request, 'board/by_rubric.html', locals()) 
